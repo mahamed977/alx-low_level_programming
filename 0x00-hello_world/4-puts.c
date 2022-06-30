7fbc65726d09a36ce631a723cd2ff97e8e5523b0
mahamed977 <mahamedhassen371@gmail.com>
2022-06-30 14:41:07 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+nclude <stdio.h>
+
+/**
+ *  * main - entry point
+ *   *
+ *    * Return: Always 0 (success)
+ *     */
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
