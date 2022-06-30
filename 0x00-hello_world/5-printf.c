69e614d3d6ae7e768e708c48b8638972597284b9
mahamed977 <mahamedhassen371@gmail.com>
2022-06-30 14:42:31 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *  * main - entry point
+ *   *
+ *    * Return: Always 0 (success)
+ *     */
+int main(void)
+{
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+			return (0);
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+P
