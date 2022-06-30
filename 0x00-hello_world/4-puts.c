b2a83d3755c4260be965e86486bd951ff6667ace
mahamed977 <mahamedhassen371@gmail.com>
2022-06-30 15:30:36 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,11 @@
-nclude <stdio.h>
-
+#include <stdio.h>
 /**
- *  * main - entry point
- *   *
- *    * Return: Always 0 (success)
- *     */
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
