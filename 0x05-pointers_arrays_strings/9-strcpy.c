4f669b59e19e5a36e7339f91162b26cd3dcfb720
mahamed977 <mahamedhassen371@gmail.com>
2022-07-11 08:32:56 -0700

    task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -5,8 +5,9 @@
  * including the terminating null byte (\0)
  * to the buffer pointed to by dest
  * @dest: pointer to the buffer in which we copy the string
- *
  * @src: string to be copied
+ *
+ * Return: the pointer to dest
  */
 char *_strcpy(char *dest, char *src)
 {
